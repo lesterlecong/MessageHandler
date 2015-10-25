9dfc8c1bf4d4020351a5d652f0978d70ab3862b2
lester_lecong <hgg.lester.lecong@gmail.com>
2015-10-26 07:04:17 +0800

    Format message to send on client

--- CouchbaseMessageHandler.java
+++ CouchbaseMessageHandler.java
@@ -1,26 +1,39 @@
-package messagehandler;
+package com.lesterprojects.messagehandler;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CouchbaseMessageHandler implements MessageHandler {
 	
+	private List<String> m_messages;
+	
 	public CouchbaseMessageHandler(){
-		
+		m_messages = new ArrayList<String>();
+		addMessage("Hello World this lester from earth can you reply on my message through skype, mail or facebook?");
+		addMessage("Hello World this lester from earth receive this message or else.....nothing :D");//sample
+	
 	}
 	
 	public void handleMessage(String message){
 		System.out.println("Handle message: " + message);
+		
 	}
 	
 	public void addMessage(String message){
 		
+		NumberFormat formatter = new DecimalFormat("000");
+		String messageSize = formatter.format(message.length());
+		message = messageSize + message;
+		System.out.println(message);
+		m_messages.add(message);
 	}
 	public List<String> messages(){
-		return new ArrayList<String>();
+		return m_messages;
 	}
 	
 	public void clearMessages(){
-		
+		m_messages.clear();
 	}
 }
