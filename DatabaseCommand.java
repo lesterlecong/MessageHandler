c4ae6eb97d572fd76e034d152880c1b00ce4310a
lester_lecong <hgg.lester.lecong@gmail.com>
2015-11-02 05:48:43 +0800

    Add DatabaseCommand class to avoid redundancy in creating/getting type of database command

---
+++ DatabaseCommand.java
@@ -0,0 +1,9 @@
+package com.lesterprojects.messagehandler;
+
+public class DatabaseCommand {
+	public static final String CREATE = "CREATE";
+	public static final String READ = "READ";
+	public static final String UPDATE = "UPDATE";
+	public static final String DELETE = "DELETE";
+	public static final String REPLICATE = "REPLICATE";
+}
