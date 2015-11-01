ed976e95dc8a73b3a9e7c295e37e0b68494f70ff
lester_lecong <hgg.lester.lecong@gmail.com>
2015-11-02 05:44:32 +0800

    Add addMessageParser method on interface

--- MessageHandler.java
+++ MessageHandler.java
@@ -2,8 +2,11 @@ package com.lesterprojects.messagehandler;
 
 import java.util.List;
 
+import com.lesterprojects.messageparser.MessageParser;
+
 public interface MessageHandler {
 	public void handleMessage(String message);
+	public void addMessageParser(MessageParser messageParser);
 	public void addMessage(String message);
 	public List<String> messages();
 	public void clearMessages();
