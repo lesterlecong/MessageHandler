eb678e8635644b05bbaf0f3e7c2587b023374d46
lester_lecong <hgg.lester.lecong@gmail.com>
2015-11-02 05:49:11 +0800

    Add Error messages for Database

---
+++ DatabaseErrorMessages.java
@@ -0,0 +1,10 @@
+package com.lesterprojects.messagehandler;
+
+public class DatabaseErrorMessages {
+	public static final String DEBUG_ERROR_001_000 = "DEBUG ERROR 001:000: Host Name not found.";
+	public static final String DEBUG_ERROR_001_001 = "DEBUG ERROR 001:001: Port Name not found.";
+	public static final String DEBUG_ERROR_001_002 = "DEBUG ERROR 001:002: Database Name not found";
+	public static final String DEBUG_ERROR_001_003 = "DEBUG ERROR 001:003: Document/Table Name not found.";
+	public static final String DEBUG_ERROR_001_004 = "DEBUG ERROR 001:004: Command Type not found";
+	public static final String DEBUG_ERROR_001_005 = "DEBUG ERROR 001:005: Data not found";
+}
